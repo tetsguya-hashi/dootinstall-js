015dce11cd8898985fc0ac6b47cc04c6c372f5fb
tetsuya-hashi <web-partner@tainer-web-partner.com>
2022-08-07 17:00:39 +0900

    コミット

--- svelteJS/src/store.js
+++
@@ -1,6 +0,0 @@
-import { writable } from "svelte/store";
-import Cookies from "js-cookie";
-//cookieにuidがあればCookieのuidを使う
-const cookie = Cookies.get('uid');
-console.log('Cookie in uid = ' + cookie);
-export const userId = writable(cookie ? cookie : null);
